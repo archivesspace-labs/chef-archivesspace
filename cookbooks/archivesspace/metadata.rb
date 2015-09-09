2bbe81304dbf79cef54c5ca845eb8723b0ead3eb
Ubuntu <ubuntu@ip-172-31-7-189.us-west-1.compute.internal>
2015-09-09 20:47:17 +0000

    updating..

---
+++ cookbooks/archivesspace/metadata.rb
@@ -0,0 +1,10 @@
+name             'archivesspace'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures chef-archivesspace'
+long_description 'Installs/Configures chef-archivesspace'
+version          '0.1.0'
+
+#depends 'zip'
+depends 'java'
